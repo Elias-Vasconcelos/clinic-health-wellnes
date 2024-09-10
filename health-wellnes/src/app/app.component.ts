e54a2e9f7b8b282cbfdcd71f13160c87e338d9a9
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-09-10 20:03:03 -0300

    Ajuste da responsividade do  header e hero

---
+++ health-wellnes/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeroComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'health-wellnes';
+}
